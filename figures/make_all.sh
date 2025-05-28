e5d81567bf5e7219b6e8d35b6e7e861ff687793f
Bernard Field <51108483+bfield1@users.noreply.github.com>
2025-05-27 17:40:20 -0700

    Make PNG and EPS versions of layer_class_sankey
    Converted from the PDF. Better than messing around with Plotly.

--- figures/make_all.sh
+++ figures/make_all.sh
@@ -3,3 +3,8 @@
 # in the PDF files of most of these plots.
 # I delete those extra pages manually in Preview.
 ./get_stats.py --sankey -H --stacked --bar --font Optima,Arial
+# To get a png, run
+# magick -density 600 layer_class_sankey.pdf layer_class_sankey.png
+# To get eps, run
+# pdftops layer_class_sankey.pdf layer_class_sankey.ps
+# ps2eps layer_class_sankey.ps
